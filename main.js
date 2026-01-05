d2fa34f3124531938c2ea383d4c4be76b5cd7d40
moymoy1l <mochi0901201@gmail.com>
2026-01-05 22:41:02 +0700

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ video.addEventListener("play", () => {
         window.location.href = "https://otieu.com/4/10181511";
     }, 15000);
 });
-const targetURL = "https://s.shopee.co.id/6puAFdql7p";
+const targetURL = "https://otieu.com/4/10338085";
 let opened = false;
 
 const videoOverlay = document.getElementById("videoOverlay");
@@ -34,6 +34,7 @@ if (videoOverlay) {
         this.remove(); // setelah klik pertama, video bisa dikontrol normal
     });
 }
+
 
 
 
